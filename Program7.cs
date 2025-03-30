120faa8bdd1023f9719b31c38c931a28b98f1713
fatemehshateri <www.rezaiiroza@gmail.com>
2025-03-30 15:37:02 -0700

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b,sum=0;
+            char r;
+            a = int.Parse(Console.ReadLine());
+            r = Console.ReadKey().KeyChar;
+            b= int.Parse(Console.ReadLine());
+            if (r == '*')
+                sum = a * b;
+            else if (r == '/')
+                sum = a / b;
+            else if (r == '+')
+                sum = a + b;
+            else if (r == '-')
+                sum = a - b;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
